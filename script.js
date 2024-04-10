5dc7504183f1b8f631010b5874d6d8a7aaf01d68
JiWoo-Yoo <selfnum_1357@naver.com>
2024-04-10 17:56:23 +0900

    Feat: 가위바위보 UI 추가 및 점수와 결과 계산 기능 추가

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   choice = "";
   rspNum = Math.floor(Math.random() * 3) + 1;
-  //console.log(rspNum);
   if (rspNum === 1) {
     choice = "rock";
   } else if (rspNum === 2) {
@@ -13,30 +15,125 @@ function getComputerChoice() {
   return choice;
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "tie";
-  } else if (playerSelection === "rock") {
-    if (computerSelection === "scissors") {
-      return "playerWins";
-    } else {
-      return "computerWins";
+function paintScore(result, playerSelection, computerSelection) {
+  const playerLabel = document.querySelector(".user-score");
+  const computerLabel = document.querySelector(".com-score");
+  playerLabel.textContent = `You: ${playerSelection} || score: ${playerScore}`;
+  computerLabel.textContent = `Computer: ${computerSelection} || score: ${computerScore}`;
+
+  if (playerScore === 5 || computerScore === 5) {
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+    const playResult = document.createElement("h1");
+    if (playerScore === computerScore) {
+      // 동점으로 끝남
+      playResult.textContent = "Tie! 동점으로 끝났습니다!";
+    } else if (playerScore === 5) {
+      // 사용자가 이김
+      playResult.textContent = `You win! ${playerScore} : ${computerScore}`;
+    } else if (computerScore === 5) {
+      // 컴퓨터가 이김
+      playResult.textContent = `You lose! ${playerScore} : ${computerScore}`;
     }
-  } else if (playerSelection === "scissors") {
-    if (computerSelection === "paper") {
-      return "playerWins";
-    } else {
-      return "computerWins";
+    document.body.appendChild(playResult);
+  } else {
+    const container = document.querySelector(".container");
+    const oldPlayResult = document.querySelector(".play-result");
+    if (oldPlayResult) {
+      oldPlayResult.classList.remove("play-result");
+      container.removeChild(oldPlayResult);
     }
-  } else if (playerSelection === "paper") {
-    if (computerSelection === "rock") {
-      return "playerWins";
-    } else {
-      return "computerWins";
-    }
+    const newplayResult = document.createElement("h2");
+    newplayResult.textContent = result + "!";
+    newplayResult.classList.add("play-result");
+    container.appendChild(newplayResult);
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playRound(playerSelection, computerSelection) {
+  let result = "";
+  // check result
+  if (playerSelection === computerSelection) {
+    result = "tie";
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      result = "playerWins";
+    } else {
+      result = "computerWins";
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      result = "playerWins";
+    } else {
+      result = "computerWins";
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      result = "playerWins";
+    } else {
+      result = "computerWins";
+    }
+  }
+  // add score
+  if (result === "computerWins") {
+    computerScore += 1;
+  } else if (result === "playerWins") {
+    playerScore += 1;
+  }
+
+  paintScore(result, playerSelection, computerSelection);
+}
+
+function handleRSPBtnClick(event) {
+  playRound(event.target.textContent, getComputerChoice());
+}
+
+const h1 = document.createElement("h1");
+const container = document.createElement("div");
+container.classList.add("container");
+const scoreDiv = document.createElement("div");
+const btnDiv = document.createElement("div");
+const resultDiv = document.createElement("div");
+
+// divs inside of scoreDiv
+const userScoreDiv = document.createElement("div");
+const userScoreLabel = document.createElement("label");
+userScoreLabel.classList.add("user-score");
+userScoreLabel.textContent = `You: none || score: ${playerScore}`;
+userScoreDiv.appendChild(userScoreLabel);
+scoreDiv.appendChild(userScoreDiv);
+
+const comScoreDiv = document.createElement("div");
+const comScoreLabel = document.createElement("label");
+comScoreLabel.classList.add("com-score");
+comScoreLabel.textContent = `Computer: none || score: ${computerScore}`;
+comScoreDiv.appendChild(comScoreLabel);
+scoreDiv.appendChild(comScoreDiv);
+
+// divs inside of btnDiv
+const rockBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+
+rockBtn.textContent = "rock";
+scissorsBtn.textContent = "scissors";
+paperBtn.textContent = "paper";
+
+rockBtn.addEventListener("click", handleRSPBtnClick);
+scissorsBtn.addEventListener("click", handleRSPBtnClick);
+paperBtn.addEventListener("click", handleRSPBtnClick);
+
+btnDiv.appendChild(rockBtn);
+btnDiv.appendChild(scissorsBtn);
+btnDiv.appendChild(paperBtn);
+
+// divs inside of resultDiv
+
+h1.textContent = "Rock Scissors Paper";
+document.body.appendChild(h1);
+
+container.appendChild(scoreDiv);
+container.appendChild(btnDiv);
+container.appendChild(resultDiv);
+
+document.body.appendChild(container);
