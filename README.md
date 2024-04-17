c4e24dc211e1e7f8d00fa8808eaed039a5c04acc
JiWoo-Yoo <selfnum_1357@naver.com>
2024-04-17 16:37:03 +0900

    CSS, 시작 및 결과페이지, README.md 수정

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# odin-rsp
+# Rock Scissors Paper
 
-- odin project의 세 번째 과제.
-- javaScript를 이용한 간단한 가위바위보 기능 및 ui 구현
+- odin project의 과제 중 하나
+- html5, css3, javaScript(ES6)를 이용한 간단한 가위바위보 기능 및 ui 구현
 
-![odin-rsp](https://github.com/JiWoo-Yoo/odin-rsp/assets/145994347/8c1639e2-5bed-41b2-8c12-21c3a252fe7c)
+## 페이지 1. 내기 점수 입력 페이지
 
-- 기능: player와 computer 둘 중 하나라도 5점 달성 시 종료
+![odin-rsp-1](https://github.com/JiWoo-Yoo/odin-rsp/assets/145994347/f0a9599f-bede-4105-8cb2-30b7d7775557)
+
+## 페이지 2. 가위바위보 진행 페이지
+
+![odin-rsp-2](https://github.com/JiWoo-Yoo/odin-rsp/assets/145994347/4d22cb2e-1657-44ab-963b-674139a021cc)
+
+## 페이지 3. 결과 페이지
+
+![odin-rsp-3](https://github.com/JiWoo-Yoo/odin-rsp/assets/145994347/a7649d99-e317-4d7e-ba8e-d67ea7498139)
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+let finScore;
 
 function getComputerChoice() {
   choice = "";
@@ -15,125 +16,255 @@ function getComputerChoice() {
   return choice;
 }
 
-function paintScore(result, playerSelection, computerSelection) {
-  const playerLabel = document.querySelector(".user-score");
-  const computerLabel = document.querySelector(".com-score");
-  playerLabel.textContent = `You: ${playerSelection} || score: ${playerScore}`;
-  computerLabel.textContent = `Computer: ${computerSelection} || score: ${computerScore}`;
+function showResult() {
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
 
-  if (playerScore === 5 || computerScore === 5) {
-    const container = document.querySelector(".container");
-    container.innerHTML = "";
-    const playResult = document.createElement("h1");
-    if (playerScore === computerScore) {
-      // 동점으로 끝남
-      playResult.textContent = "Tie! 동점으로 끝났습니다!";
-    } else if (playerScore === 5) {
-      // 사용자가 이김
-      playResult.textContent = `You win! ${playerScore} : ${computerScore}`;
-    } else if (computerScore === 5) {
-      // 컴퓨터가 이김
-      playResult.textContent = `You lose! ${playerScore} : ${computerScore}`;
-    }
-    document.body.appendChild(playResult);
+  const playResult = document.createElement("h1");
+  const resultScore = document.createElement("h1");
+  const resultBtnDiv = document.createElement("div");
+  const replayBtn = document.createElement("button");
+  const selectScoreBtn = document.createElement("button");
+  playResult.classList.add("result-text");
+  resultScore.classList.add("result-score");
+  resultBtnDiv.classList.add("result-btn-div");
+  if (playerScore === computerScore) {
+    // 동점으로 끝남
+    playResult.textContent = "🤝🏻Tie! 동점으로 끝났습니다!🤝🏻";
+  } else if (playerScore === finScore) {
+    // 사용자가 이김
+    playResult.textContent = "🎉You win!🎉";
+  } else if (computerScore === finScore) {
+    // 컴퓨터가 이김
+    playResult.textContent = "🤦🏻‍♀️You lose!🤦🏻‍♂️";
+  }
+
+  resultScore.textContent = `${playerScore} : ${computerScore}`;
+  replayBtn.textContent = "Replay";
+  selectScoreBtn.textContent = "Choose Max Score";
+
+  replayBtn.addEventListener("click", () => {
+    playerScore = 0;
+    computerScore = 0;
+    document.body.innerHTML = "";
+    startGame(finScore);
+  });
+  selectScoreBtn.addEventListener("click", () => {
+    playerScore = 0;
+    computerScore = 0;
+    document.body.innerHTML = "";
+    finScore = 0;
+    selectScore();
+  });
+
+  container.appendChild(playResult);
+  container.appendChild(resultScore);
+  resultBtnDiv.appendChild(replayBtn);
+  resultBtnDiv.appendChild(selectScoreBtn);
+  container.appendChild(resultBtnDiv);
+}
+function paintScore(result) {
+  const userScore = document.querySelector(".user-score");
+  const uScore = userScore.querySelector(".score");
+  const comScore = document.querySelector(".com-score");
+  const cScore = comScore.querySelector(".score");
+  uScore.textContent = `${playerScore}`;
+  cScore.textContent = `${computerScore}`;
+  if (playerScore === finScore || computerScore === finScore) {
+    showResult();
   } else {
-    const container = document.querySelector(".container");
-    const oldPlayResult = document.querySelector(".play-result");
-    if (oldPlayResult) {
-      oldPlayResult.classList.remove("play-result");
-      container.removeChild(oldPlayResult);
-    }
-    const newplayResult = document.createElement("h2");
-    newplayResult.textContent = result + "!";
-    newplayResult.classList.add("play-result");
-    container.appendChild(newplayResult);
+    const playResult = document.querySelector(".play-result");
+    playResult.textContent = result + "!";
+  }
+}
+
+function paintRSP(userHand, pcHand) {
+  const userHandImg = document.querySelector(".user-hand");
+  const pcHandImg = document.querySelector(".pc-hand");
+
+  if (userHand === "rock") {
+    userHandImg.textContent = "✊🏻";
+  } else if (userHand === "scissors") {
+    userHandImg.textContent = "✌🏻";
+  } else if (userHand === "paper") {
+    userHandImg.textContent = "✋🏻";
+  }
+
+  if (pcHand === "rock") {
+    pcHandImg.textContent = "✊🏻";
+  } else if (pcHand === "scissors") {
+    pcHandImg.textContent = "✌🏻";
+  } else if (pcHand === "paper") {
+    pcHandImg.textContent = "✋🏻";
   }
 }
 
 function playRound(playerSelection, computerSelection) {
+  paintRSP(playerSelection, computerSelection);
   let result = "";
   // check result
+
   if (playerSelection === computerSelection) {
-    result = "tie";
+    result = "😅tie";
   } else if (playerSelection === "rock") {
     if (computerSelection === "scissors") {
-      result = "playerWins";
+      result = "🤩Win";
     } else {
-      result = "computerWins";
+      result = "😥Lose";
     }
   } else if (playerSelection === "scissors") {
     if (computerSelection === "paper") {
-      result = "playerWins";
+      result = "🤩Win";
     } else {
-      result = "computerWins";
+      result = "😥Lose";
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      result = "playerWins";
+      result = "🤩Win";
     } else {
-      result = "computerWins";
+      result = "😥Lose";
     }
   }
   // add score
-  if (result === "computerWins") {
+  if (result === "😥Lose") {
     computerScore += 1;
-  } else if (result === "playerWins") {
+  } else if (result === "🤩Win") {
     playerScore += 1;
   }
 
-  paintScore(result, playerSelection, computerSelection);
+  paintScore(result);
 }
 
 function handleRSPBtnClick(event) {
-  playRound(event.target.textContent, getComputerChoice());
+  playRound(event.target.name, getComputerChoice());
 }
 
-const h1 = document.createElement("h1");
-const container = document.createElement("div");
-container.classList.add("container");
-const scoreDiv = document.createElement("div");
-const btnDiv = document.createElement("div");
-const resultDiv = document.createElement("div");
+function startGame(inputScore) {
+  finScore = inputScore;
+  const h1 = document.createElement("h1");
+  const container = document.createElement("div");
+  const playResult = document.createElement("div");
+  container.classList.add("container");
+  playResult.classList.add("play-result");
+  const scoreDiv = document.createElement("div");
+  const mainBoardDiv = document.createElement("div");
+  const playerHandDiv = document.createElement("div");
+  const btnDiv = document.createElement("div");
+  const pcHandDiv = document.createElement("div");
+  const resultDiv = document.createElement("div");
 
-// divs inside of scoreDiv
-const userScoreDiv = document.createElement("div");
-const userScoreLabel = document.createElement("label");
-userScoreLabel.classList.add("user-score");
-userScoreLabel.textContent = `You: none || score: ${playerScore}`;
-userScoreDiv.appendChild(userScoreLabel);
-scoreDiv.appendChild(userScoreDiv);
+  // divs inside of scoreDiv
+  scoreDiv.classList.add("score-container");
+  const userScoreDiv = document.createElement("div");
+  const userName = document.createElement("div");
+  const userScore = document.createElement("div");
+  userScoreDiv.classList.add("user-score");
+  userName.classList.add("name");
+  userScore.classList.add("score");
+  userName.textContent = "You";
+  userScore.textContent = `${playerScore}`;
+  userScoreDiv.appendChild(userName);
+  userScoreDiv.appendChild(userScore);
+  scoreDiv.appendChild(userScoreDiv);
 
-const comScoreDiv = document.createElement("div");
-const comScoreLabel = document.createElement("label");
-comScoreLabel.classList.add("com-score");
-comScoreLabel.textContent = `Computer: none || score: ${computerScore}`;
-comScoreDiv.appendChild(comScoreLabel);
-scoreDiv.appendChild(comScoreDiv);
+  scoreDiv.appendChild(playResult); // 판마다의 결과
 
-// divs inside of btnDiv
-const rockBtn = document.createElement("button");
-const scissorsBtn = document.createElement("button");
-const paperBtn = document.createElement("button");
+  const comScoreDiv = document.createElement("div");
+  const comName = document.createElement("div");
+  const comScore = document.createElement("div");
+  comScoreDiv.classList.add("com-score");
+  comName.classList.add("name");
+  comScore.classList.add("score");
+  comName.textContent = "Computer";
+  comScore.textContent = `${computerScore}`;
+  comScoreDiv.appendChild(comName);
+  comScoreDiv.appendChild(comScore);
+  scoreDiv.appendChild(comScoreDiv);
 
-rockBtn.textContent = "rock";
-scissorsBtn.textContent = "scissors";
-paperBtn.textContent = "paper";
+  // divs of main board
+  mainBoardDiv.classList.add("main-board");
 
-rockBtn.addEventListener("click", handleRSPBtnClick);
-scissorsBtn.addEventListener("click", handleRSPBtnClick);
-paperBtn.addEventListener("click", handleRSPBtnClick);
+  // divs of hand img
+  playerHandDiv.classList.add("user-hand");
+  pcHandDiv.classList.add("pc-hand");
 
-btnDiv.appendChild(rockBtn);
-btnDiv.appendChild(scissorsBtn);
-btnDiv.appendChild(paperBtn);
+  playerHandDiv.textContent = "You";
+  pcHandDiv.textContent = "PC";
 
-// divs inside of resultDiv
+  // divs inside of btnDiv
+  btnDiv.classList.add("btn-div");
+  const rockBtn = document.createElement("button");
+  const scissorsBtn = document.createElement("button");
+  const paperBtn = document.createElement("button");
 
-h1.textContent = "Rock Scissors Paper";
-document.body.appendChild(h1);
+  rockBtn.textContent = "rock✊🏻";
+  scissorsBtn.textContent = "scissors✌🏻";
+  paperBtn.textContent = "paper🖐🏻";
 
-container.appendChild(scoreDiv);
-container.appendChild(btnDiv);
-container.appendChild(resultDiv);
+  rockBtn.name = "rock";
+  scissorsBtn.name = "scissors";
+  paperBtn.name = "paper";
 
-document.body.appendChild(container);
+  rockBtn.addEventListener("click", handleRSPBtnClick);
+  scissorsBtn.addEventListener("click", handleRSPBtnClick);
+  paperBtn.addEventListener("click", handleRSPBtnClick);
+
+  btnDiv.appendChild(rockBtn);
+  btnDiv.appendChild(scissorsBtn);
+  btnDiv.appendChild(paperBtn);
+
+  mainBoardDiv.appendChild(playerHandDiv);
+  mainBoardDiv.appendChild(btnDiv);
+  mainBoardDiv.appendChild(pcHandDiv);
+
+  // divs inside of resultDiv
+
+  h1.textContent = "Rock, Scissors, Paper!";
+  document.body.appendChild(h1);
+
+  container.appendChild(scoreDiv);
+  container.appendChild(mainBoardDiv);
+  container.appendChild(resultDiv);
+
+  document.body.appendChild(container);
+}
+
+// 맨 첫 화면(승리 점수 고르는 화면)
+function selectScore() {
+  const startLabel = document.createElement("div");
+  const inputForm = document.createElement("div");
+  const inputLabel = document.createElement("label");
+  const input = document.createElement("input");
+
+  startLabel.textContent = "Start";
+  startLabel.classList.add("start-label");
+  inputForm.classList.add("input-form");
+  inputLabel.setAttribute("for", "fin-score");
+  inputLabel.textContent = "Max Score:";
+  input.setAttribute("autofocus", "true");
+  input.setAttribute("type", "number");
+  input.setAttribute("id", "fin-score");
+  input.setAttribute("value", 1);
+  input.setAttribute("min", 1);
+  input.setAttribute("max", 10);
+
+  document.body.appendChild(startLabel);
+  inputForm.appendChild(inputLabel);
+  inputForm.appendChild(input);
+  document.body.appendChild(inputForm);
+
+  startLabel.addEventListener("click", () => {
+    const inputScore = Number(input.value);
+    if (inputScore < 1 || inputScore > 10) {
+      alert("Invalid Input! Please Enter Score 1~10.");
+    } else {
+      startLabel.remove();
+      inputLabel.remove();
+      input.remove();
+      inputForm.remove();
+      startGame(inputScore);
+    }
+  });
+}
+
+selectScore();
