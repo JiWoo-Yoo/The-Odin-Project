58dd1208329df9dc739256d7a3f0cfe6a4fea389
JiWoo-Yoo <selfnum_1357@naver.com>
2024-07-17 18:00:23 +0900

    [Feat] Add rendering

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,23 +1,53 @@
 import config from './apikey.js';
 const API_KEY = config.apikey;
 
-const latitude = document.querySelector('#latitude');
-const longitude = document.querySelector('#longitude');
+const city = document.querySelector('#city');
 const btn = document.querySelector('#see-weather');
 
-async function logJSONData(lat, long) {
+function paintWeather(weatherData, cityName) {
+    const weather = document.querySelector('#weather');
+    weather.innerHTML = '';
+
+    const country = weatherData.location.country;
+    const condition = weatherData.current.condition.text;
+    const icon_url = weatherData.current.condition.icon;
+    const temperature = weatherData.current.temp_c;
+
+    const position = document.createElement('h2');
+    position.textContent = `${cityName} of ${country}: `;
+
+    const condition_text = document.createElement('h1');
+    condition_text.textContent = `${condition}`;
+
+    const icon = document.createElement('img');
+    icon.src = icon_url;
+
+    const temp = document.createElement('p');
+    temp.textContent = `Temperature: ${temperature}°C`;
+
+    weather.appendChild(position);
+    weather.appendChild(condition_text);
+    weather.appendChild(icon);
+    weather.appendChild(temp);
+}
+async function logJSONData(cityName) {
+    if (!cityName) {
+        alert('Please enter a city name');
+        return;
+    }
     try {
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${lat},${long}`);
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}`);
         if (!response.ok) {
+            console.log(cityName);
+            console.log(response);
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+ 
         const json = await response.json();
-        console.log(json);
+        paintWeather(json, cityName);
     } catch (error) {
         console.log(error);
     }
 }
-const lat_arg = Number(latitude.value);
-const lon_arg = Number(longitude.value);
 
-btn.addEventListener('click', () => logJSONData(lat_arg), lon_arg);
+btn.addEventListener('click', () => logJSONData(city.value));
